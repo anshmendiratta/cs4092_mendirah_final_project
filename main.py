c7f840bcddaad04bd3b5d9a83ef041229bf30268
anshmendiratta <anshmendiratta2023@gmail.com>
2026-07-31 01:16:09 -0400

    [feat] add remaining tables and destroy_tables func

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import src.utilities as ut
 
 def main() -> None:
     connection, cursor = ut.init_db_connection()
-    ut._create_tables(cursor)
+
+    cursor.execute("PRAGMA foreign_keys = ON;")  # Enable FKs.
+
+    ut.destroy_tables(cursor)
+    connection.commit()
+
+    ut.create_tables(cursor)
     connection.commit()
 
     ut.destroy_db_connection(cursor)
--- src/utilities.py
+++ src/utilities.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import List
 
 
 def init_db_connection() -> [sqlite3.Connection, sqlite3.Cursor]:
@@ -10,20 +11,77 @@ def destroy_db_connection(cursor: sqlite3.Cursor) -> None:
     cursor.close()
 
 
-def _create_tables(cursor: sqlite3.Cursor) -> None:
+# NOTE: needed to overwrite previous saves to the table.
+def destroy_tables(cursor: sqlite3.Cursor) -> None:
+    tables: List = cursor.fetchall()
+
+    for table in tables:
+        name = table[0]
+        cursor.execute(f'DROP TABLE IF EXISTS "{name}";')
+
+
+def create_tables(cursor: sqlite3.Cursor) -> None:
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Customer (
-        CustomerID INTEGER PRIMARY KEY,
-        Name TEXT,
-        Email TEXT
-    );
+        CREATE TABLE Customer (
+            CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            FirstName TEXT NOT NULL,
+            LastName TEXT NOT NULL,
+            Email TEXT NOT NULL UNIQUE,
+            Phone TEXT
+        );
     """)
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Product (
-        ProductID INTEGER PRIMARY KEY,
-        Name TEXT,
-        Price DECIMAL(8,2),
-        Stock INTEGER
-    );
+        CREATE TABLE Staff (
+            StaffID INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            FirstName TEXT NOT NULL,
+            LastName TEXT NOT NULL,
+            Position TEXT NOT NULL
+        );
+     """)
+
+    cursor.execute("""
+        CREATE TABLE Product (
+            ProductID INTEGER PRIMARY KEY AUTOINCREMENT,
+            StaffID INTEGER NOT NULL,
+
+            Name TEXT NOT NULL,
+            Description TEXT,
+            Price REAL NOT NULL
+                CHECK (Price >= 0),
+            Stock INTEGER NOT NULL DEFAULT 0
+                CHECK (Stock >= 0),
+
+            FOREIGN KEY (StaffID) REFERENCES Staff(StaffID)
+        );
+     """)
+
+    cursor.execute("""
+        CREATE TABLE CreditCard (
+            CardID INTEGER PRIMARY KEY AUTOINCREMENT,
+            CustomerID INTEGER NOT NULL,
+
+            CardNumber TEXT NOT NULL UNIQUE,
+            ExpirationMonth INTEGER NOT NULL
+                CHECK (ExpirationMonth BETWEEN 1 AND 12),
+            ExpirationYear INTEGER NOT NULL,
+            CardholderName TEXT NOT NULL,
+
+            FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+        );
+    """)
+
+    cursor.execute("""
+        CREATE TABLE Purchase (
+            PurchaseID INTEGER PRIMARY KEY AUTOINCREMENT,
+            CustomerID INTEGER NOT NULL,
+            CardID INTEGER NOT NULL,
+
+            PurchaseDate TEXT NOT NULL,
+
+            FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+            FOREIGN KEY (CardID) REFERENCES CreditCard(CardID)
+        );
     """)
